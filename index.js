b9bf2dc8ee5d3d5793cb75eb7242d9f55225988e
Kyle D'Amato <kyledamato4@gmail.com>
2022-03-15 19:17:14 -0400

    completed exercises in index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let dognames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dognames.length; i++){
+	dognames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'red'
+
